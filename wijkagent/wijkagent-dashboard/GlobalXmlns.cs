9a62f732508ef9f89a37126807b81c8612fbb58f
Semdeb <semdeboerdere@gmail.com>
2025-11-19 12:03:52 +0100

    .net9 project setup RAZOR

--- wijkagent/wijkagent-dashboard/GlobalXmlns.cs
+++
@@ -1,2 +0,0 @@
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "wijkagent_dashboard")]
-[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "wijkagent_dashboard.Pages")]
