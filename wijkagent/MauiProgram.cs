4135a75cd4fdb6c8f64efdfef9ab86d6fbcb1d00
Semdeb <semdeboerdere@gmail.com>
2025-12-16 13:14:32 +0100

    API connectie

--- wijkagent/MauiProgram.cs
+++ wijkagent/MauiProgram.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using WijkAgent.Core.Data;
 using WijkAgent.Core.Services;
 
@@ -28,9 +30,38 @@ public static class MauiProgram
         builder.Services.AddDbContext<WijkAgentDbContext>(options =>
             options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
+        // notifier singleton
+        builder.Services.AddSingleton<CrimeNotifier>();
+
         // Services
         builder.Services.AddScoped<ICrimeService, CrimeService>();
 
-        return builder.Build();
+        // HttpClient factory for API polling
+        builder.Services.AddHttpClient();
+
+        // Register the API poll service as singleton and inject notifier
+        builder.Services.AddSingleton<ApiPollService>(sp =>
+        {
+            var httpFactory = sp.GetRequiredService<IHttpClientFactory>();
+            var db = sp.GetRequiredService<WijkAgentDbContext>();
+            var crimeService = sp.GetRequiredService<ICrimeService>();
+            var logger = sp.GetRequiredService<ILogger<ApiPollService>>();
+            var notifier = sp.GetRequiredService<CrimeNotifier>();
+
+            // api endpoint
+            var apiUrl = "https://service.p2000.page/api-HZFUUVBsdXprUBmcjlHhh1D1cdktrUjl";
+
+            // Poll every 10 seconds
+            var pollInterval = TimeSpan.FromSeconds(10);
+
+            return new ApiPollService(httpFactory, db, crimeService, notifier, logger, apiUrl, pollInterval);
+        });
+
+        var app = builder.Build();
+
+        // start polling
+        app.Services.GetRequiredService<ApiPollService>();
+
+        return app;
     }
 }
