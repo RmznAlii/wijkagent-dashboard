3466c6429e22b7e7637cffca110cbc1070887999
Nalawi Woldeyohannes <nalawi.woldeyohannes@hva.nl>
2025-12-19 13:37:53 +0100

    xUnit tests voor tc4-03,04 geschreven en uitgevoerd. De test cases waren geslaagd.

---
+++ WijkAgent.Tests/Services/FakeSocialMediaService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WijkAgent.Core.Models;
+using WijkAgent.Core.Services;
+
+namespace WijkAgent.Tests.Services
+{
+    public class FakeSocialMediaService : ISocialMediaService
+    {
+        private readonly List<SocialMediaPost> _posts;
+
+        public FakeSocialMediaService(List<SocialMediaPost> posts)
+        {
+            _posts = posts;
+        }
+
+        public Task<IReadOnlyList<SocialMediaPost>> GetPostsForIncidentAsync(
+            DateTime incidentTime,
+            IEnumerable<string> keywords)
+        {
+            var result = _posts
+                .Where(p =>
+                    p.PostedAt >= incidentTime &&
+                    p.PostedAt <= incidentTime.AddMinutes(30))
+                .ToList();
+
+            return Task.FromResult<IReadOnlyList<SocialMediaPost>>(result);
+        }
+
+        public Task<IReadOnlyList<SocialMediaPost>> GetPostsForIncidentAsync(DateTime incidentTime, IEnumerable<string> keywords, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
